0296f58cc9448b1671e30a91323f3ae4a70222bd
Eric Romrell <eric.romrell@walmart.com>
2021-10-14 20:30:25 -0700

    Implemented test for firebase

--- src/firebase_client.py
+++ src/firebase_client.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 import firebase_admin
 from firebase_admin import auth
 
 
 class FirebaseClient:
     @staticmethod
-    def get_firebase_user(token): pass
+    def get_firebase_user(token: Optional[str]): pass
 
 
 class FirebaseClientImpl(FirebaseClient):
@@ -12,5 +14,5 @@ class FirebaseClientImpl(FirebaseClient):
         firebase_admin.initialize_app()
 
     @staticmethod
-    def get_firebase_user(token):
+    def get_firebase_user(token: Optional[str]):
         return auth.verify_id_token(token)
---
+++ test/firebase_client_test.py
@@ -0,0 +1,31 @@
+import os
+import unittest
+
+from firebase_admin.auth import InvalidIdTokenError
+
+from test import properties
+from firebase_client import FirebaseClientImpl
+
+class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../src/firebase_creds.json"
+        cls.client = FirebaseClientImpl()
+
+    def test_validate_token(self):
+        # Test null token
+        self.assertRaises(ValueError, lambda: self.client.get_firebase_user(None))
+
+        # Test empty token
+        self.assertRaises(ValueError, lambda: self.client.get_firebase_user(""))
+
+        # Test partially correct format
+        self.assertRaises(InvalidIdTokenError, lambda: self.client.get_firebase_user("a.bad.token"))
+
+        # Test valid but old
+        self.assertRaises(InvalidIdTokenError, lambda: self.client.get_firebase_user(properties.old_firebase_token))
+
+        # In order to run this test, you'll have to generate a new valid token and place it in the properties file
+        user = self.client.get_firebase_user(properties.valid_firebase_token)
+        self.assertIsNotNone(user)
+        print(user)
