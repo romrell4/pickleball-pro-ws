bb870b98b6cacdc260d6251fc65ec7846c555c39
Eric Romrell <eric.romrell@walmart.com>
2021-10-28 10:47:02 -0700

    Removed date formatter

--- src/handler.py
+++ src/handler.py
@@ -82,8 +82,6 @@ def format_response(body=None, status_code=200):
 def default_serialize(x):
     if isinstance(x, datetime.datetime):
         return x.strftime("%Y-%m-%dT%H:%M:%S%z")
-    elif isinstance(x, datetime.date):
-        return x.isoformat()
     elif isinstance(x, DomainBase):
         return x.to_dict()
     else:
